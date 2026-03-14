b8982f04c4d1dae4a911b3117c9a7b21c1c44f1d
hemanth2k7-commits <hemanthkumar2k7@gmail.com>
2026-03-14 13:06:10 +0530

    New=>GitHub Profile Viewer

---
+++ GitHub_profile_viewer/script.js
@@ -0,0 +1,50 @@
+document.getElementById("user").addEventListener("keydown", function (event) {
+  //Calls getuser when enter key is pressed
+  if (event.key === "Enter") {
+    getUser();
+  }
+});
+async function getUser() {
+    document.getElementById("profile-card").style.display="none";//To clear previous result
+  document.getElementById("output").textContent = ""; //To clear the previous error message
+  const user = document.getElementById("user").value.trim();
+  if (!user) {
+    document.getElementById("output").textContent = "Please enter a Username";
+    return;
+  }
+  try {
+    const url = `https://api.github.com/users/${user}`;
+    const userfetch = await fetch(url).then(document.getElementById("output").textContent="Loading....");
+    const reposfetch=await fetch(`${url}/repos`);//return repos by user
+    if (!userfetch.ok) {
+      throw new Error("User not found");
+    }
+    document.getElementById("profile-card").style.display="flex";//Get all elements with className and change value
+    //Main profile part
+    const data = await userfetch.json().then(document.getElementById("output").textContent="");
+    document.getElementById("profilepic").src = data.avatar_url;
+    document.getElementById("name").textContent =
+      `Name:${data.name || "No Name available"}`;
+    document.getElementById("bio").textContent =
+      `Bio:${data.bio || "No bio available"}`;
+    document.getElementById("followers").textContent =
+      `Followers:${data.followers}`;
+    document.getElementById("following").textContent =
+      `Following:${data.following}`;
+    document.getElementById("repos").textContent =
+      `Public Repos:${data.public_repos}`;
+
+    //Profile repo part
+    const repodata=await reposfetch.json();
+    repodata.forEach(repo => {
+        if (repo.stargazers_count<=10) {
+            const item=document.createElement("p");
+        item.textContent=`Repo Name: ${repo.name}\nLanguage(mainly): ${repo.language||"No language available"}\n🌟${repo.stargazers_count}`;
+        document.getElementById("repoList").appendChild(item);
+        }
+    });
+  } catch (error) {
+    document.getElementById("output").textContent = `Error: ${error.message}`;
+    document.getElementById("profile-card").style.display="none";
+  }
+}
